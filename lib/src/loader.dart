9c34007c6b28c499ceda5f62ff8d5334531d070a
akshdhillon <9teen9t7@gmail.com>
2023-07-30 21:48:08 +0530

    [ADD] added loader stack

--- lib/src/loader.dart
+++ lib/src/loader.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
 
 class ElegantLoader extends StatelessWidget {
-  const ElegantLoader({Key? key}) : super(key: key);
+  const ElegantLoader({
+    Key? key,
+    required this.child,
+    this.alignment = AlignmentDirectional.topStart,
+  }) : super(key: key);
+
+  final Widget child;
+  final AlignmentGeometry alignment;
 
   @override
   Widget build(BuildContext context) {
-    return  CircularProgressIndicator();
+    return Stack(
+      alignment: alignment,
+      children: [
+        child,
+        AbsorbPointer(
+          child: Container(
+            height: double.maxFinite,
+            width: double.maxFinite,
+            color: Colors.black.withAlpha(150),
+            child: Center(
+              child: CircularProgressIndicator(),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
